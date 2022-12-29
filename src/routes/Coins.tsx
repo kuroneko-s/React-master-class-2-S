7271abf016dcdafb33e78900795a54c15b2fcd75
최동혁 <Developer5>
2022-12-29 17:54:46 +0900

    chap5 진행중

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,9 +1,80 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Title = styled.h1`
   color: ${(props) => props.theme.accentColor};
+  font-size: 3rem;
 `;
 
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const CoinList = styled.ul``;
+
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  margin-bottom: 10px;
+  padding: 20px;
+  border-radius: 20px;
+  transition: color 0.1s ease-in;
+  &:hover {
+    color: ${(props) => props.theme.accentColor};
+  }
+`;
+
+// https://api.coinpaprika.com/v1/coins
+const coinList = [
+  {
+    id: "btc-bitcoin",
+    name: "Bitcoin",
+    symbol: "BTC",
+    rank: 1,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "eth-ethereum",
+    name: "Ethereum",
+    symbol: "ETH",
+    rank: 2,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "usdt-tether",
+    name: "Tether",
+    symbol: "USDT",
+    rank: 3,
+    is_new: false,
+    is_active: true,
+    type: "token",
+  },
+];
+
 export default function Coins() {
-  return <Title>Coins</Title>;
+  return (
+    <Container>
+      <Header>
+        <Title>코인</Title>
+      </Header>
+      <CoinList>
+        {coinList.map((coin) => (
+          <Link to={`/${coin.id}`}>
+            <Coin key={coin.id}>{coin.name} &rarr;</Coin>
+          </Link>
+        ))}
+      </CoinList>
+    </Container>
+  );
 }
